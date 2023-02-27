3f2598c0e4f185f180764564d72b2a4ef89d9a8c
Madalina-Eleonora Gheorghe <madalinaeleonora.gheorghe@gmail.com>
2023-02-27 20:43:25 +0200

    update

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import * as CONSTANTS from 'src/assets/CONSTANTS';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-
+  public CONSTANTS: any = CONSTANTS;
 }
