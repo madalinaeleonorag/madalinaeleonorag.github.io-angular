ce37bd8042d3a045478764f5acc65bb419a30a0e
Madalina-Eleonora Gheorghe <madalinaeleonora.gheorghe@gmail.com>
2023-03-02 13:50:26 +0200

    updated meta information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,12 +30,51 @@ export class AppComponent {
   public isSeeLessTestimonials: boolean = true;
   public isSeeLessProjects: boolean = true;
   slideConfig = {
+    lazyLoad: 'ondemand',
     slidesToShow: 3,
     slidesToScroll: 1,
     infinite: true,
     autoplay: true,
     autoplaySpeed: 4000,
     arrows: true,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 0,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   constructor(private externalService: ExternalService) {}
