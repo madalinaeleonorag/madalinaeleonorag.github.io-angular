eccd148fbf56aa4e2936f2b024148001143cad63
Madalina-Eleonora Gheorghe <madalinaeleonora.gheorghe@gmail.com>
2024-12-07 15:57:54 +0200

    updated project view

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { GitHubProject } from 'src/app/interfaces/github-project';
 import { CommonService } from 'src/app/services/common.service';
+import { ImageDialogComponent } from './image-dialog/image-dialog.component';
 
 @Component({
   selector: 'app-project',
@@ -11,35 +13,38 @@ import { CommonService } from 'src/app/services/common.service';
 export class ProjectComponent implements OnInit {
   @Input() project: GitHubProject;
 
-  public imageUrl = './../../../assets/image-not-found.png';
   public languagesPercentage: any = {};
 
-  constructor(private commonService: CommonService, private http: HttpClient) {}
+  constructor(private commonService: CommonService, private http: HttpClient, public dialog: MatDialog) { }
 
   ngOnInit() {
-    this.commonService.returnImageURL(this.project.imageUrl).then((res) => {
-      this.imageUrl = res;
-    });
     this.getRepoLanguagePercentage();
   }
 
   public projectDescription(): string {
     return this.project?.description?.length > 90
       ? (
-          this.project?.description?.slice(
-            0,
-            this.project?.description?.slice(0, 90).lastIndexOf(' ')
-          ) + ' [...]'
-        )?.trim()
+        this.project?.description?.slice(
+          0,
+          this.project?.description?.slice(0, 90).lastIndexOf(' ')
+        ) + ' [...]'
+      )?.trim()
       : this.project?.description?.trim();
+  }
+
+  public openImage(): void {
+    this.dialog.open(ImageDialogComponent, {
+      data: this.project.imageUrl,
+      maxHeight: '80vh',
+    });
   }
 
   private getRepoLanguagePercentage() {
     this.http
       .get(
         'https://api.github.com/repos/madalinaeleonorag/' +
-          this.project.name +
-          '/languages'
+        this.project.name +
+        '/languages'
       )
       .subscribe((responseObj: any) => {
         const totalPtsArr = Object.values(responseObj);
